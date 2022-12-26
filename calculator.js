553c16505ae513399d76166164f91dcc40a23914
muhithfahim <muhithfahim12@gmail.com>
2022-12-26 00:07:23 -0500

    add all files 26102022

---
+++ calculator.js
@@ -0,0 +1,35 @@
+//jshint version: 6
+const express = require('express');
+const bodyParser =require("body-parser");
+
+const app = express();
+const port = 3000;
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get('/', function(req, res)  {
+  res.sendFile(__dirname + "/index.html")
+});
+ app.post("/",function(req,res){
+ 
+  var num1 =Number(req.body.num1);
+  var num2 =Number(req.body.num2);
+  var result=num1+num2;
+
+
+  res.send("the result of your calculation is "+result)
+ });
+ app.get('/bmicalculator',function(req,res)  {
+  res.sendFile(__dirname +"/bmicalculator.html")
+ });
+ app.post("/bmicalculator",function(req,res){
+
+  var weight = parseFloat(req.body.Weight);
+  var height= parseFloat(req.body.height);
+  var bmi=weight/(height*height);
+
+  res.send("Your BMI is "+ bmi );
+ });
+ 
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
